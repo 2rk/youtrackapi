c3daac62d0bfa611203d51ec14fbe403998e2bb3
Nigel Rausch <nigel.rausch@tworedkites.com>
2013-10-30 15:31:14 +1000

    Fixed issue with not calculating estimates in epic

--- meme.rb
+++ meme.rb
@@ -1,30 +1,31 @@
 require 'CSV'
 require_relative 'youtrack'
 
+project_id = 'pwi'
+
 i = Youtrack.new
-i.authenticate 'test', 'test'
-#p i.create_ticket 'test', summary: 'hellomeme'
-#
-#exit
+i.authenticate '2rk', '2rk'
 
 first = true
 parent_issue = nil
 parent_feature = ""
+epic_hide = false
+
 CSV.foreach('import.csv') do |row|
   unless first
-    feature = row[0]
+    epic = row[0]
     summary = row[1]
     hours = row[2]
     hide = row[3]
     task_type = 'task' if row[4]
 
-    if feature
-      parent_issue = i.create_ticket 'test', private: hide, type: task_type, estimation: hours, summary: feature
-      parent_feature = feature
+    if epic
+      parent_issue = i.create_ticket project_id, private: hide, type: 'epic', estimation: hours, summary: epic
+      parent_feature = epic
+      epic_hide = hide
     elsif summary
-      i.create_ticket 'test', private: hide, type: task_type, estimation: hours, summary: "#{summary} [#{parent_feature}]", subtask_of: parent_issue
+      i.create_ticket project_id, private: hide || epic_hide, type: task_type, estimation: hours, summary: "#{summary} [#{parent_feature}]", subtask_of: parent_issue
     end
-
   end
   first = false
 
--- youtrack.rb
+++ youtrack.rb
@@ -5,15 +5,25 @@ class Youtrack
   def initialize
     @connection = Faraday.new(:url => 'http://tracker.tworedkites.com') do |faraday|
       faraday.request  :url_encoded             # form-encode POST params
-      faraday.response :logger                  # log requests to STDOUT
+      #faraday.response :logger                  # log requests to STDOUT
       faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
     end
   end
 
   def authenticate login, password
-    login = @connection.post '/youtrack/rest/user/login', { login: 'test', password: 'test'}
+    login = @connection.post '/youtrack/rest/user/login', { login: login, password: password}
     @cookie = login.headers['set-cookie']
   end
+
+  def create_project id, name
+    create_response = @connection.put "/youtrack/rest/admin/project/#{id}" do |req|
+      req.headers['Cookie'] = @cookie
+      req.body = {projectName: name, startingNumber: 1, projectLeadLogin: 'test'}
+    end
+    raise create_response.env[:body] unless create_response.status == 201
+
+  end
+
 
   # create_ticket
   #   project_name    short name of project
@@ -30,11 +40,12 @@ class Youtrack
     p options
     options.merge!(permittedGroup: '2rk') if options[:private]
     options.merge!(project: project_name)
+    estimation = options[:estimation]
 
     command_lists = []
     command_lists << "subtask of #{options[:subtask_of]}" if options[:subtask_of]
     command_lists << "type #{options[:type] || 'feature'}"
-    command_lists << "estimation #{options[:estimation]}h" if options[:estimation]
+    #command_lists << "estimation #{options[:estimation]}h" if options[:estimation]
     command_list = command_lists.join(' ')
 
     issue_options = options.delete_if {|key, value| ![:project, :summary, :description, :permittedGroup].include? key }
@@ -53,13 +64,21 @@ class Youtrack
       req.headers['Cookie'] = @cookie
       req.body = {command: command_list}
     end
+    if estimation
+      estimate_response = @connection.post do |req|
+        req.url "/youtrack/rest/issue/#{new_ticket}/execute"
+        req.headers['Cookie'] = @cookie
+        req.body = {command: "estimation #{estimation}h"}
+      end
+
+    end
     unless command_response.status == 200
       delete_response = @connection.delete "/youtrack/rest/issue/#{new_ticket}" do |req|
         req.headers['Cookie'] = @cookie
       end
       raise command_response.env[:body]
     end
-
+    puts "Ticket #{new_ticket} created"
     new_ticket
   end
 
